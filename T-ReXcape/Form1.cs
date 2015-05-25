6096d9b9a4e76fd11bb55f76452a60e7d263b68e
Andy <andyheckel1253@yaho.de>
2015-05-25 20:19:10 +0200

    Kommentare hinzugefügt Kleineren Bug beim sezten und speichern der Objekte behoben.

--- T-ReXcape/Form1.cs
+++ T-ReXcape/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 // only for debug
 using System.Diagnostics;
@@ -21,21 +22,24 @@ namespace T_ReXcape
         Point mousePosition;
         Control dragDropObject = null;
 
+        // save all posible objects on ma in this Dictionary
         Dictionary<string, Dictionary<string, string>> objects;
 
         public Form1()
         {
             InitializeComponent();
             objects = new Dictionary<string, Dictionary<string, string>>();
+            // init all posible objects
             objects["player1start"] = new Dictionary<string, string>();
             objects["player1start"]["backGround"] = "Blue";
             objects["player1start"]["width"] = "30";
             objects["player1start"]["height"] = "30";
-            objects["player1start"]["maxOnPanel"] = "1";
+            objects["player1start"]["maxOnPanel"] = "3";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // @TODO
             Map map = new Map();
             // open file dialog to select map file
             //openFileDialog1.ShowDialog();
@@ -43,6 +47,9 @@ namespace T_ReXcape
             //map.setMapFileName(openFileDialog1.FileName);
         }
 
+        /**
+         * updates map size
+         */
         private void updateSettings()
         {
             int mapHeight = Convert.ToInt16(mapheight.Value);
@@ -57,6 +64,7 @@ namespace T_ReXcape
             updateSettings();
         }
         
+        // add object
         private void addPlayer1Start(object sender, EventArgs e)
         {
             String type = "player1start";
@@ -66,6 +74,7 @@ namespace T_ReXcape
             }
         }
 
+        // prepares object to add to panel
         private Panel preparePanelObject(String type)
         {
             Panel panel = new Panel();
@@ -73,11 +82,12 @@ namespace T_ReXcape
             panel.Height = Convert.ToInt16(objects[type]["height"]);
             panel.BackColor = Color.FromName(objects[type]["backGround"]);
             panel.Location = mousePosition;
-            panel.Name = type;
+            panel.Name = type + countObjectOnPanel(type);
             panel.Click += new System.EventHandler(dragDropMouseClick);
             return panel;
         }
 
+        // onclick drag will be activated, on second click deactivated
         private void dragDropMouseClick(Object sender, EventArgs e)
         {
             if (sender.GetType().IsSubclassOf(typeof(Control)))
@@ -93,6 +103,27 @@ namespace T_ReXcape
             }
         }
 
+        // moves locked object with mouse move
+        private void mapPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragDropObject != null)
+            {
+                dragDropObject.Location = getPosition(e.Location);
+                // @TODO remove after develop
+                Debug.WriteLine("X:" + dragDropObject.Location.X + " Y:" + dragDropObject.Location.Y);
+            }
+        }
+
+        // gets position depends on blocksize. position should be allways in one block
+        private Point getPosition(Point p)
+        {
+            int x = (p.X / blockSize) * blockSize;
+            int y = (p.Y / blockSize) * blockSize;
+            return new Point(x, y);
+        }
+
+
+        // opens context rightclick on map
         private void mapPanel_MouseClick(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Right)
@@ -102,42 +133,34 @@ namespace T_ReXcape
             }
         }
 
-        private void mapPanel_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (dragDropObject != null)
-            {
-                dragDropObject.Location = getPosition(e.Location);
-                Debug.WriteLine("X:" + dragDropObject.Location.X + " Y:" + dragDropObject.Location.Y);
-            }
-        }
-
-        private Point getPosition(Point p)
-        {
-            int x = (p.X / blockSize) * blockSize;
-            int y = (p.Y / blockSize) * blockSize;
-            return new Point(x, y);
-        }
-
+        /* @TODO remove after testing
+         * saves all objects positions in ini file
+         */
         private void speichernToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            foreach (Control child in mapPanel.Controls)
+            saveFileDialog1.ShowDialog();
+            if (!saveFileDialog1.FileName.Equals(""))
             {
-                saveFileDialog1.ShowDialog();
-                if(!saveFileDialog1.FileName.Equals(""))
+                IniFile file = new IniFile(saveFileDialog1.FileName);
+                foreach (Control child in mapPanel.Controls)
                 {
-                    IniFile file = new IniFile(saveFileDialog1.FileName);
-                    file.IniWriteValue("map", child.Name + ".x", child.Location.X.ToString());
-                    file.IniWriteValue("map", child.Name + ".y", child.Location.Y.ToString());
+                    String name = child.Name;
+                    file.IniWriteValue("map", name + ".x", child.Location.X.ToString());
+                    file.IniWriteValue("map", name + ".y", child.Location.Y.ToString());
                 }
             }
         }
 
+        /**
+         * counts all objects on panel with same name
+         */
         private int countObjectOnPanel(String name)
         {
             int result = 0;
             foreach (Control child in mapPanel.Controls)
             {
-                if (child.Name.Equals(name))
+                // remove last diggit to count all with same name
+                if (Regex.Replace(child.Name, @"\d$", "").Equals(name))
                 {
                     result++;
                 }
