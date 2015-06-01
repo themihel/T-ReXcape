0fbbb2c45d584aa786d533b93b8760bebaedc25e
themihel <helfenstein.mischa@gmail.com>
2015-06-01 18:45:03 +0200

    Added comments to "Item" class

--- T-ReXcape/Item.cs
+++ T-ReXcape/Item.cs
@@ -7,6 +7,7 @@ namespace T_ReXcape
 {
     class Item
     {
+        // variables
         private String key;
         private String background;
         private Int32 maxOnPanel;
@@ -14,6 +15,12 @@ namespace T_ReXcape
         private Int32 width;
         private Int32 height;
         
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="_key">Key of item</param>
+        /// <param name="_width">Width of item</param>
+        /// <param name="_height">Height of item</param>
         public Item(String _key,
                     Int32 _width,
                     Int32 _height)
@@ -23,16 +30,28 @@ namespace T_ReXcape
             height = _height;
         }
 
+        /// <summary>
+        /// Sets name of item
+        /// </summary>
+        /// <param name="_name">Name of item</param>
         public void setName(String _name)
         {
             name = _name;
         }
 
+        /// <summary>
+        /// Sets background of item
+        /// </summary>
+        /// <param name="_background">Background filename related to ressources</param>
         public void setBackground(String _background)
         {
             background = _background;
         }
 
+        /// <summary>
+        /// Sets maximum amount of objects on panel // 0 = disabled
+        /// </summary>
+        /// <param name="_maxOnPanel">Maximum number of items</param>
         public void setMaxOnPanel(Int32 _maxOnPanel)
         {
             // check if number is equal or greater than 0 (Note: 0 == disabled)
@@ -43,31 +62,49 @@ namespace T_ReXcape
             
         }
 
+        /// <summary>
+        /// Returns item-key
+        /// </summary>
         public String getKey()
         {
             return key;
         }
 
+        /// <summary>
+        /// Returns maximum amount of items on panel
+        /// </summary>
         public Int32 getMaxOnPanel()
         {
             return maxOnPanel;
         }
 
+        /// <summary>
+        /// Returns width of item
+        /// </summary>
         public Int32 getWidth()
         {
             return width;
         }
 
+        /// <summary>
+        /// Returns height of item
+        /// </summary>
         public Int32 getHeight()
         {
             return height;
         }
 
+        /// <summary>
+        /// Returns background filname related to ressources
+        /// </summary>
         public String getBackground()
         {
             return background;
         }
 
+        /// <summary>
+        /// Returns item-name
+        /// </summary>
         public String getName()
         {
             return name;
