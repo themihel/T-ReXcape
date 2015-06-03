2b9d0cf19d0ff7b27675681233bdf9a090f99358
themihel <helfenstein.mischa@gmail.com>
2015-06-03 23:01:19 +0200

    Added Util class

---
+++ T-ReXcape/Util.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace T_ReXcape
+{
+    static class Util
+    {
+        /// <summary>
+        /// Calculates exact position depending on point and blockSize
+        /// </summary>
+        /// <param name="p">MousePoint</param>
+        /// <param name="blockSize">Size of a map block</param>
+        /// <returns>Returns accurate position</returns>
+        static public Point getAccuratePosition(Point p, Int32 blockSize)
+        {
+            int x = (p.X / blockSize) * blockSize;
+            int y = (p.Y / blockSize) * blockSize;
+            return new Point(x, y);
+        }
+
+        /// <summary>
+        /// Removes digits from String
+        /// </summary>
+        /// <param name="s">Parse string</param>
+        /// <returns>Returns string without digits</returns>
+        static public String removeDigitsFromString(String s)
+        {
+            return Regex.Replace(s, @"\d$", "");
+        }
+    }
+}
