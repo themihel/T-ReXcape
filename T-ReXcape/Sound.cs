3a80ddc9e27149ac718a85f224d319def5e565f8
themihel <helfenstein.mischa@gmail.com>
2015-06-12 17:45:37 +0200

    Small refactor of sound class
    - added comments

--- T-ReXcape/Sound.cs
+++ T-ReXcape/Sound.cs
@@ -8,6 +8,67 @@ namespace T_ReXcape
 {
     class Sound
     {
+        // variables
+        SoundPlayer player;
+
+        /// <summary>
+        /// Init player
+        /// </summary>
+        public Sound()
+        {
+            player = new SoundPlayer();
+        }
+
+        /// <summary>
+        /// set track to soundtrack
+        /// </summary>
+        public void setSoundtrack()
+        {
+            player.Stream = Properties.Resources.trexcape_soundtrack;
+        }
+
+        /// <summary>
+        /// Play track once;
+        /// </summary>
+        public void playOnce()
+        {
+            
+        }
+
+        /// <summary>
+        /// Play track in loop
+        /// </summary>
+        public void playLoop()
+        {
+            if (isReadyToPlay())
+            {
+                player.PlayLooping();
+            }
+        }
+
+        /// <summary>
+        /// Stop track
+        /// </summary>
+        public void stop()
+        {
+            if (isReadyToPlay())
+            {
+                player.Stop();
+            }
+        }
+
+        /// <summary>
+        /// checks if player is ready to play
+        /// </summary>
+        private Boolean isReadyToPlay()
+        {
+            // @TODO more validation?
+            return player.Stream != null ? true : false;
+        }
+
+        /// <summary>
+        /// Play Boom sound once
+        /// </summary>
         public static void playBoom()
         {
             SoundPlayer player = new SoundPlayer();
